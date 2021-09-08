169803dbe05f4ee5935c66bb3d0defc1bfa4a42c
tiana25 <tianati01@gmail.com>
2021-09-08 13:34:41 +0300

    nav links & css

--- marioplan/src/components/auth/SignIn.js
+++ marioplan/src/components/auth/SignIn.js
@@ -34,7 +34,7 @@ class SignIn extends Component {
                         <input type="password" id = "password" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
-                        <button className="btn pinklighten-1 z-depth-0">Login</button>
+                        <button className="btn pink lighten-1 z-depth-0">Login</button>
                     </div>
                 </form>
             </div>
