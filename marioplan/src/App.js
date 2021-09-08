2ba40c253f3fa723a6736d17030dc99a26222148
tiana25 <tianati01@gmail.com>
2021-09-08 12:14:11 +0300

    login & signup components

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 class App extends Component {
   render(){
@@ -14,6 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path = '/' component = {Dashboard}></Route>
             <Route path = '/project/:id' component = {ProjectDetails}></Route>
+            <Route path = '/signin' component={SignIn}></Route>
+            <Route path ='/signup' component={SignUp}></Route>
           </Switch>
         </div>
       </BrowserRouter>
