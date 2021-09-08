2c5921de7f128861c4c2402d87679003a97a0978
tiana25 <tianati01@gmail.com>
2021-09-08 14:44:43 +0300

    using thunk

--- marioplan/src/components/projects/CreateProjects.js
+++ marioplan/src/components/projects/CreateProjects.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 
 class CreateProject extends Component {
@@ -17,7 +19,8 @@ class CreateProject extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state)
     }
 
     render() {
@@ -42,4 +45,10 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -9,6 +9,10 @@ const initState = {
 
 
 const projectReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'CREATE_PROJECT': console.log('created project', action.project)
+            break;
+    }
     return state;
 }
 
