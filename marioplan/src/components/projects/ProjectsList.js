a39c1cb75370498913dfcd864d8c6563f1ff69b8
tiana25 <tianati01@gmail.com>
2021-09-08 11:12:16 +0300

    dashboard component

---
+++ marioplan/src/components/projects/ProjectsList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ProjectsList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Project Title</span>
+                    <p>Posted by Tania</p>
+                    <p className="grey-text">8th September, 10am</p>
+                </div>   
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Project Title</span>
+                    <p>Posted by Tania</p>
+                    <p className="grey-text">8th September, 10am</p>
+                </div>   
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Project Title</span>
+                    <p>Posted by Tania</p>
+                    <p className="grey-text">8th September, 10am</p>
+                </div>   
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectsList
